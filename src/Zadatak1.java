97494feac525036532590e9df0a5c7295a453ab1
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-01-14 00:34:19 +0100

    Zadatak1 update

--- src/Zadatak1.java
+++ src/Zadatak1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Zadatak1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesite ime:");
+        String ime = s.nextLine();
+        System.out.println("Unesite prezime:");
+        String prezime = s.nextLine();
+
+        System.out.println(ime + " " + prezime);
+
+
+
+    }
 }
