525d759c8c4e038179ad0379e7ef82a63350e260
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-01-14 00:57:25 +0100

    Zadatak2 račun

---
+++ src/Zadatak2.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+        int [] array = {2, 5, 6, 5};
+        deljenjeSa5(array);
+        
+    }
+
+    public static double deljenjeSa5 (int [] array) {
+        double deljenje = array [0]/5;
+        double deljenje1 = array [1]/5;
+        double deljenje2 = array [2]/5;
+        double deljenje3 = array [3]/5;
+
+       double deljenjeX = deljenje + deljenje1 + deljenje2 + deljenje3;
+                return deljenjeX;
+    }
+
+}
