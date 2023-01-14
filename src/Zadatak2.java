4990be9b2b9965b70177c7dda363e445b5e4454f
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-01-14 01:04:36 +0100

    Zadatak2 račun dodatak

--- src/Zadatak2.java
+++ src/Zadatak2.java
@@ -4,7 +4,11 @@ public class Zadatak2 {
     public static void main(String[] args) {
         int [] array = {2, 5, 6, 5};
         deljenjeSa5(array);
-        
+        System.out.println(deljenjeSa5(array));
+
+        System.out.println(mnozenjeSa5(array));
+
+
     }
 
     public static double deljenjeSa5 (int [] array) {
@@ -15,6 +19,16 @@ public class Zadatak2 {
 
        double deljenjeX = deljenje + deljenje1 + deljenje2 + deljenje3;
                 return deljenjeX;
+    }
+
+    public static double mnozenjeSa5 (int [] array) {
+        double deljenje = array [0]*5;
+        double deljenje1 = array [1]*5;
+        double deljenje2 = array [2]*5;
+        double deljenje3 = array [3]*5;
+
+        double deljenjeX = deljenje + deljenje1 + deljenje2 + deljenje3;
+        return deljenjeX;
     }
 
 }
