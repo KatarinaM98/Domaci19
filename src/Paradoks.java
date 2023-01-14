cecf37b6a45157f5e11e82c3da710ad05d2ea179
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-01-14 01:28:49 +0100

    PARADOKS

---
+++ src/Paradoks.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Paradoks {
+    public static void main(String[] args) {
+        System.out.println("Kako zapasti u paradoks?");
+
+
+                Scanner s = new Scanner(System.in);
+                System.out.println("Answer is not true?");
+                boolean result = s.nextBoolean();
+                if (result) {
+                    System.out.println("Answer is true");
+
+                } else {
+                    System.out.println("Answer is not true");
+                    }
+        System.out.println();
+        System.out.println("Upali ste u paradoks");
+                ;
+
+                }
+            }
+
+
+
